86f3febda6f9cab4bbf7f8e37f2145611e53bc64
janselalb <21jansel@gmail.com>
2024-09-08 12:18:09 -0400

    Config gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jansellopez.github.io',
+  base: '/blog-preview-card',
   integrations: [tailwind()]
 });
