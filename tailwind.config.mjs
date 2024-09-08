839bc08a39b9e66b929ec52a171250089d58031e
janselalb <21jansel@gmail.com>
2024-09-08 12:15:51 -0400

    First commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors:{
+			yellow:'#F4D04E',
+			gray:{
+				500:'#6B6B6B',
+				950:'#111111'
+			},
+			white:'#FFFFFF'
+		},
+		extend: {},
+	},
+	plugins: [],
+}
